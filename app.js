f7c9bd744e4694e69f583b10fd02877c3b05643f
Juan Cruz Lima <juan.c.lima@live.com>
2022-05-30 21:36:34 -0300

    fix heroku

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app       = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('server levantando');
-});
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
